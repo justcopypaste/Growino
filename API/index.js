b8e5b2f6c6aeffe5c4c69d51b3b3838b3c9a0a3d
Joako <joacocaballero1@gmail.com>
2024-02-13 23:02:04 -0300

    back

--- API/index.js
+++ API/index.js
@@ -9,7 +9,6 @@ const fs = require("fs");
 const http = require("http");
 const https = require("https");
 const subdomain = require("express-subdomain")
-const tls = require('tls');
 
 app.use(cors());
 
@@ -30,50 +29,22 @@ app.use(subdomain('api', viewRoutes));
 // HTTP Server
 const httpServer = http.createServer(app);
 
+// HTTPS Server
+const privateKey = fs.readFileSync(
+  __dirname + "/public/ssl/root/private.key",
+  "utf8"
+);
+const certificate = fs.readFileSync(
+  __dirname + "/public/ssl/root/certificate.crt",
+  "utf8"
+);
+const credentials = { key: privateKey, cert: certificate };
+const httpsServer = https.createServer(credentials, app);
+
 httpServer.listen(80, () => {
   console.log(`HTTP Server running on port 80`);
 });
 
-// Configuration for each subdomain
-const subdomains = {
-  'api.growino.app': {
-    key: fs.readFileSync(__dirname + "/public/ssl/api/private.key", "utf8"),
-    cert: fs.readFileSync(__dirname + "/public/ssl/api/certificate.crt", "utf8")
-  },
-  'growino.app': {
-    key: fs.readFileSync(__dirname + "/public/ssl/root/private.key", "utf8"),
-    cert: fs.readFileSync(__dirname + "/public/ssl/root/certificate.crt", "utf8")
-  },
-};
-
-// Create HTTPS server
-const server = https.createServer((req, res) => {
-  // Extract the requested subdomain from the request
-  const host = req.headers.host;
-  const subdomain = host.split('.')[0]; // Assuming subdomain is the first part of the hostname
-
-  // Find the configuration for the requested subdomain
-  const config = subdomains[host];
-
-  if (config) {
-    // If a matching configuration is found, use it to handle the request
-    const options = {
-      key: config.key,
-      cert: config.cert
-    };
-    const subdomainServer = https.createServer(options, (req, res) => {
-      res.writeHead(200);
-      res.end(`Hello from ${subdomain}`);
-    });
-    subdomainServer.emit('request', req, res);
-  } else {
-    // If no matching configuration is found, respond with a 404 Not Found
-    res.writeHead(404);
-    res.end('Not Found');
-  }
-});
-
-// Start the server
-server.listen(443, () => {
+httpsServer.listen(443, () => {
   console.log(`HTTPS Server running on port 443`);
 });
