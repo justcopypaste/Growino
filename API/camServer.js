db4a3faddad184a9f27323fd01aed00e7a7752b1
Joako <joacocaballero1@gmail.com>
2024-03-17 20:46:44 -0300

    cam port

--- API/camServer.js
+++ API/camServer.js
@@ -2,8 +2,8 @@ const Stream = require('node-rtsp-stream')
 
 new Stream({
     name: 'growino cam 0',
-    streamUrl: 'rtsp://www.growino.app:554/videoMain',
-    wsPort: 3000,
+    streamUrl: 'rtsp://www.growino.app:3000/videoMain',
+    wsPort: 3001,
     ffmpegOptions: {
       '-stats': '',
       '-r': 30
