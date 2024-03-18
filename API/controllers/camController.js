d64a2222ee390e130d10b1114ccb6576b972aa45
Joako <joacocaballero1@gmail.com>
2024-03-17 22:19:09 -0300

    get cam ip

--- API/controllers/camController.js
+++ API/controllers/camController.js
@@ -1,9 +1,10 @@
-const {Cam} = require('../models/cam')
+const { Cam } = require('../models/cam')
 const db = require('../database')
+const dns = require('dns');
 
 const updateCam = (req, res) => {
     const cam = new Cam(req.body.id, req.body.ip)
-    db.updateCam(cam).then((r)=>{
+    db.updateCam(cam).then((r) => {
         res.send(r)
     }).catch((err) => {
         res.send(err)
@@ -12,7 +13,7 @@ const updateCam = (req, res) => {
 
 const createCam = (req, res) => {
     const cam = new Cam(req.body.id, req.body.ip)
-    db.createCam(cam).then((r)=>{
+    db.createCam(cam).then((r) => {
         res.send(r)
     }).catch((err) => {
         res.send(err)
@@ -20,15 +21,26 @@ const createCam = (req, res) => {
 }
 
 const deleteCam = (req, res) => {
-    db.deleteCam(req.body.id).then((r)=>{
+    db.deleteCam(req.body.id).then((r) => {
         res.send(r)
     }).catch((err) => {
         res.send(err)
     })
 }
 
+const getCamIP = (req, res) => {
+    dns.lookup("www.growino.app", (err, address) => {
+        if (err) {
+            res.json({ error: 'DNS lookup failed', details: err });
+        } else {
+            res.json({ ip: address });
+        }
+    });
+}
+
 module.exports = {
     createCam,
     updateCam,
-    deleteCam
+    deleteCam,
+    getCamIP
 }
--- API/routes/api/camRoutes.js
+++ API/routes/api/camRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.get('/create', camController.createCam)
 router.get('/update', camController.updateCam)
 router.get('/delete', camController.deleteCam)
+router.get('/', camController.getCamIP)
 
 module.exports = router
