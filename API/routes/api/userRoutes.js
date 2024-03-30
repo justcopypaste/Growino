3b27f485385042ec402b21ba87983b97851b8f83
Joako <joacocaballero1@gmail.com>
2024-03-30 18:35:58 -0300

    /users -> /profile

--- API/routes/api/userRoutes.js
+++ API/routes/api/userRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post("/login", userController.login);
 router.post("/register", userController.register);
-router.get("/users", userController.getUsers);
+router.get("/profile", userController.getUsers);
 router.delete("/users", userController.deleteUser);
 
 module.exports = router;
